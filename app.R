bdc3678fa929621a4a0a99887465c8b3c5de62bd
Duncan Leng <duncan.leng@ascent.io>
2023-04-02 23:28:31 +0100

    start modularising

--- app.R
+++ app.R
@@ -17,13 +17,178 @@ questions <- paste0(
 )
 # all answers lowercase
 answers <- df$transliteration_lower
-num_questions <- length(questions)
-
 stopifnot(length(questions) == length(answers))
 
+# throw into df to be used later
+df_cyrillic_questions <- data.frame(questions = questions, answers = answers)
 
 
-ui <- fluidPage(
+
+questionUI <- function(id, title) {
+  tagList(
+    h3(title),
+    
+    hr(),
+    valueBoxOutput(outputId = NS(id, "box_question_num")),
+    valueBoxOutput(outputId = NS(id, "box_score")),
+    valueBoxOutput(outputId = NS(id, "box_time")),
+    hr(),
+    
+    fluidRow(
+      div(
+        textOutput(outputId = NS(id, "question_question")),
+        textOutput(outputId = NS(id, "question_result")),
+        br(),
+        textInput(inputId = NS(id, "question_answer"), label = "Answer: "),
+        br(),
+        column(
+          width = 6,
+          br(),
+          actionButton(inputId = NS(id, "question_submit"), label = "Submit"),
+          actionButton(inputId = NS(id, "question_next"), label = "Next"),
+          actionButton(inputId = NS(id, "question_stop"), label = "Stop"),
+          actionButton(inputId = NS(id, "question_go_reset"), label = "Reset")
+        ),
+        style = "margin:10px"
+      )
+    )
+  )
+}
+
+questionServer <- function(id, df_questions) {
+  stopifnot(!is.reactive(df_questions))
+  
+  moduleServer(id, function(input, output, session) {
+    
+    df_questions
+    
+    question_idx <- reactiveVal(value = 1)
+    question_answered <- reactiveVal(value = FALSE)
+    
+    questions_correct <- reactiveVal(value = 0)
+    questions_answered <- reactive({question_idx() - 1})
+    questions_total <- reactive({nrow(df_questions)})
+    
+    game_timer_time <- reactiveVal(value = 0)
+    game_timer_active <- reactiveVal(value = TRUE)
+    
+    
+    observeEvent(input$question_go_reset, {
+      question_idx(1)
+      questions_correct(0)
+      game_timer_time(0)
+      game_timer_active(TRUE)
+      shinyjs::enable("question_submit")
+      shinyjs::enable("question_next")
+    })
+    
+    observeEvent(input$question_stop, {
+      game_timer_active(FALSE)
+      shinyjs::disable("question_submit")
+      shinyjs::disable("question_next")
+    })
+    
+    observeEvent(input$question_next, {
+      question_idx(isolate(question_idx() + 1)) 
+      question_answered(FALSE)
+      # clear previous answer
+      updateTextInput(inputId = "question_answer", value = "")
+    })
+    
+    observe({
+      # every 1000 ms update the game_timer_time if active
+      invalidateLater(1000, session)
+      isolate({
+        if (game_timer_active()) {
+          game_timer_time(game_timer_time() + 1)
+        }
+      })
+    })
+    
+    output$box_question_num <- renderValueBox({
+      valueBox(
+        value = glue("{question_idx()}/{questions_total()}"),
+        subtitle = "Question",
+        #icon = icon("question"),
+        width = 4
+      )
+    })
+    output$box_score <- renderValueBox({
+      valueBox(
+        value = glue("{questions_correct()}/{questions_answered()}"),
+        subtitle = "Score",
+        #icon = icon("credit-card"),
+        width = 4
+      )
+    })
+    output$box_time <- renderValueBox({
+      # whenever game_timer_time() changes, this also updates
+      time <- hms(game_timer_time())
+      valueBox(
+        value = glue("{time}"),
+        subtitle = "Time",
+        width = 4
+      )
+    })
+    
+    
+    question_outcome <- eventReactive(input$question_submit, {
+      real_answer <-  df_questions$answers[question_idx()]
+      
+      if (is.null(input$question_answer)){return(-1)}
+      
+      question_answer <- tolower(input$question_answer)
+      if (!(question_answer == real_answer)){
+        if (!question_answered()) {
+          question_answered(TRUE)
+        }
+        txt <- glue("Not quite! The correct answer is {real_answer}.")
+        return(txt)
+      }
+      if (question_answer == real_answer){
+        if (!question_answered()) {
+          questions_correct(isolate(questions_correct() + 1))
+          question_answered(TRUE)
+        }
+        txt <- glue("Great! The correct answer is {real_answer}.")
+        return(txt)
+      }
+    })
+    
+    output$question_question <- renderText({
+      df_questions$questions[question_idx()]
+    })
+    
+    output$question_result <- renderText({
+      question_outcome()
+    })
+    
+  })
+}
+
+title = "Learn Bulgarian"
+ui <- navbarPage(
+  title = div(
+    img(
+      src="Flag_of_Bulgaria.png", 
+      height = 20,
+    ),
+    title
+  ), 
+  header = titlePanel(
+    title = div(
+      img(
+        src="Flag_of_Bulgaria.png", 
+        height = 40,
+        style = "margin:10px 10px"
+      ),
+      title
+    )
+  ),
+  footer = div(
+    "This app was created by Duncan Leng (March 2023).",
+    style = "margin:10px 10px"
+  ),
   
   tags$head(
     tags$link(
@@ -34,172 +199,34 @@ ui <- fluidPage(
     ),
   useShinyjs(),
   
-  fluidRow(
-    column(width = 3),
-    column(
-      width = 6,
-      
-      titlePanel(
-        title = div(
-          img(
-            src="Flag_of_Bulgaria.png", 
-            height = 50,
-            style = "margin:10px 10px"
-            ),
-          "The Learn Bulgarian Shiny App"
-          ),
-        windowTitle = "Learn Bulgarian"
-        ),
-      
-      
-      h3("Cyrillic Alphabet Quiz"),
-      
-      hr(),
-      
-      fluidRow(
-        column(width = 3, valueBoxOutput(outputId = "box_question_num", width = 12)),
-        column(width = 3, valueBoxOutput(outputId = "box_score", width = 12)),
-        column(width = 3),
-        column(width = 3, valueBoxOutput(outputId = "box_time", width = 12))
-      ),
-      
-      hr(),
-      
-      fluidRow(
-        div(
-          textOutput(outputId = "question_question"),
-          column(
-            width = 6,
-            textInput(inputId = "question_answer", label = "Answer: "),
-            br(),
-            actionButton(inputId = "question_submit", label = "Submit"),
-            actionButton(inputId = "question_next", label = "Next"),
-            actionButton(inputId = "question_stop", label = "Stop"),
-            actionButton(inputId = "question_go_reset", label = "Reset")
-          ),
-          column(
-            width = 6, 
-            textOutput(outputId = "question_result")
-          ),
-          style = "margin:10px"
-        )
-      ),
-      
-      br(),
-      hr(),
-      br(),
-      
-      p("This app was created by Duncan Leng (March 2023).")
-      
-    ) %>% tagAppendAttributes(class="main_col_class"),
-    column(width = 3)
+  tabPanel("Start"),
+  
+  navbarMenu(
+    "Learn",
+    tabPanel(
+      "Cyrillic Alphabet"
+    ),
+    tabPanel(
+      "Animals"
+    )
+  ), 
+  
+  navbarMenu(
+    "Quiz",
+    tabPanel(
+      "Cyrillic Alphabet",
+      questionUI("Question_1", title = "Cyrillic Alphabet Quiz")
+    ),
+    tabPanel(
+      "Animals",
+      questionUI("Question_2", title = "Bulgarian Animals Quiz")
+    )
   )
-  
-  
 )
 
 server <- function(input, output, session) {
-  
-  question_idx <- reactiveVal(value = 1)
-  question_answered <- reactiveVal(value = FALSE)
-  game_score <- reactiveVal(value = 0)
-  
-  game_timer_time <- reactiveVal(value = 0)
-  game_timer_active <- reactiveVal(value = TRUE)
-  
-  
-  observeEvent(input$question_go_reset, {
-    question_idx(1)
-    game_score(0)
-    game_timer_time(0)
-    game_timer_active(TRUE)
-    shinyjs::enable("question_submit")
-    shinyjs::enable("question_next")
-  })
-  
-  observeEvent(input$question_stop, {
-    game_timer_active(FALSE)
-    shinyjs::disable("question_submit")
-    shinyjs::disable("question_next")
-  })
-  
-  observeEvent(input$question_next, {
-    question_idx(isolate(question_idx() + 1)) 
-    question_answered(FALSE)
-    # clear previous answer
-    updateTextInput(inputId = "question_answer", value = "")
-  })
-  
-  
-  
-  observe({
-    # every 1000 ms update the game_timer_time if active
-    invalidateLater(1000, session)
-    isolate({
-      if (game_timer_active()) {
-        game_timer_time(game_timer_time() + 1)
-      }
-    })
-  })
-  
-  output$box_question_num <- renderValueBox({
-    valueBox(
-      value = glue("{question_idx()}/30"),
-      subtitle = "Question",
-      #icon = icon("question"),
-      width = 4
-    )
-  })
-  output$box_score <- renderValueBox({
-    valueBox(
-      value = glue("{game_score()}/30"),
-      subtitle = "Score",
-      #icon = icon("credit-card"),
-      width = 4
-    )
-  })
-  output$box_time <- renderValueBox({
-    # whenever game_timer_time() changes, this also updates
-    time <- hms(game_timer_time())
-    valueBox(
-      value = glue("{time}"),
-      subtitle = "Time",
-      width = 4
-      )
-    })
-  
-  
-  question_outcome <- eventReactive(input$question_submit, {
-    real_answer <-  answers[question_idx()]
-    if (is.null(input$question_answer)){return(-1)}
-    
-    question_answer <- tolower(input$question_answer)
-    if (!(question_answer == real_answer)){
-      if (!question_answered()) {
-        question_answered(TRUE)
-      }
-      txt <- glue("Not quite! The correct answer is {answers[question_idx()]}.")
-      return(txt)
-      }
-    if (question_answer == real_answer){
-      if (!question_answered()) {
-        game_score(isolate(game_score() + 1))
-        question_answered(TRUE)
-      }
-      txt <- glue("Great! The correct answer is {answers[question_idx()]}.")
-      return(txt)
-      }
-    })
-  
-  output$question_question <- renderText({
-    questions[question_idx()]
-    })
-  
-  output$question_result <- renderText({
-    question_outcome()
-    })
-  
-  
+  questionServer("Question_1", df_questions = df_cyrillic_questions)
+  questionServer("Question_2", df_questions = df_cyrillic_questions)
 }
 
 
