52d1e92242f2852df4caa08b168285cf8641874c
Duncan Leng <duncan.leng@ascent.io>
2023-04-20 22:35:39 +0100

    move data to data-raw and make internal

---
+++ data-raw/df_cyrillic.R
@@ -0,0 +1,19 @@
+## code to make internal vocab datasets
+
+df_cyrillic <- readr::read_csv("data-raw/bulgarian_cyrillic_alphabet.csv")
+df_numbers <- readr::read_csv("data-raw/bulgarian_numbers.csv")
+df_food <- readr::read_csv("data-raw/bulgarian_food.csv")
+df_drinks <- readr::read_csv("data-raw/bulgarian_drinks.csv")
+df_animals <- readr::read_csv("data-raw/bulgarian_animals.csv")
+df_question_words <- readr::read_csv("data-raw/bulgarian_questions.csv")
+
+usethis::use_data(
+  df_cyrillic,
+  df_numbers,
+  df_food,
+  df_drinks,
+  df_animals,
+  df_question_words,
+  overwrite = TRUE, 
+  internal = TRUE
+)
